3c367f28ea14a88d2ecdc964959778bf3c56e743
Mercy Mwova <katunge22.mercy@student.cuk.ac.ke>
2026-08-05 09:58:38 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,12 +31,12 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
 };
 
 // Enable CORS for all routes and handle preflight OPTIONS requests
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options(/(.*)/, cors(corsOptions));
 
 // 2. Parsers
 app.use(express.json());
